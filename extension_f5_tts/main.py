3b04ff565613e1f98cc1d920cf1b240a81414f2e
Roberts Slisans <rsxdalv@gmail.com>
2024-10-17 21:08:35 +0300

    init

---
+++ extension_f5_tts/main.py
@@ -0,0 +1,36 @@
+import gradio as gr
+
+
+def extension__tts_generation_webui():
+    f5_tts_ui()
+    return {
+        "package_name": "extension_f5_tts",
+        "name": "F5-TTS",
+        "version": "0.0.1",
+        "requirements": "git+https://github.com/rsxdalv/extension_f5_tts@main",
+        "description": "F5-TTS: A Fairytaler that Fakes Fluent and Faithful Speech with Flow Matching.",
+        "extension_type": "interface",
+        "extension_class": "text-to-speech",
+        "author": "Yushen Chen and Zhikang Niu and Ziyang Ma and Keqi Deng and Chunhui Wang and Jian Zhao and Kai Yu and Xie Chen",
+        "extension_author": "rsxdalv",
+        "license": "MIT",
+        "website": "https://github.com/SWivid/F5-TTS",
+        "extension_website": "https://github.com/rsxdalv/extension_f5_tts",
+        "extension_platform_version": "0.0.1",
+    }
+
+
+def f5_tts_ui():
+    with gr.Group(visible=False):
+        from f5_tts.gradio_app import app
+    app.render()
+
+
+if __name__ == "__main__":
+    if "demo" in locals():
+        locals()["demo"].close()
+    with gr.Blocks() as demo:
+        extension__tts_generation_webui()
+    demo.launch(
+        server_port=7770,
+    )
---
+++ setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+
+setuptools.setup(
+	name="extension_f5_tts",
+    packages=setuptools.find_namespace_packages(),
+	version="0.0.1",
+	author="rsxdalv",
+	description="F5-TTS: A Fairytaler that Fakes Fluent and Faithful Speech with Flow Matching.",
+	url="https://github.com/rsxdalv/extension_f5_tts",
+    project_urls={},
+    scripts=[],
+    install_requires=[
+        "gradio",
+        "f5_tts @ git+https://github.com/rsxdalv/F5-TTS@main",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
