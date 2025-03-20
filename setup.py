3f9f14ab4a67b5871ff2602f6647735c63979a14
Roberts Slisans <roberts.slisans@gmail.com>
2025-03-20 20:10:18 +0000

    switch to main repo

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setuptools.setup(
 	name="extension_f5_tts",
     packages=setuptools.find_namespace_packages(),
-	version="0.1.3",
+	version="0.1.4",
 	author="rsxdalv",
 	description="F5-TTS: A Fairytaler that Fakes Fluent and Faithful Speech with Flow Matching.",
 	url="https://github.com/rsxdalv/extension_f5_tts",
@@ -12,7 +12,7 @@ setuptools.setup(
     scripts=[],
     install_requires=[
         "gradio",
-        "f5_tts @ git+https://github.com/rsxdalv/F5-TTS@main",
+        "f5_tts @ git+https://github.com/SWivid/F5-TTS@main",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
